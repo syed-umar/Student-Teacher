3372e1277e6d10254721898ad8c10319eabce036
Umar Bkh <umar482@gmail.com>
2015-02-16 12:46:46 +0500

    quick fixes, main page

--- public/angular_app/mainpage/app.js
+++ public/angular_app/mainpage/app.js
@@ -4,32 +4,40 @@ var validationApp = angular.module('validationApp', []);
 
 // create angular controller
 validationApp.controller('mainController', function($scope, $http) {
-	
+
 });
 
-validationApp.controller('dataController', function($scope, $http) {
+validationApp.controller('dataController', function($scope, $http, $timeout) {
 
 	$scope.showClass = false;
+	$scope.displayPage = false;
 
 	$http.get('/getclassregs/' + user_id)
 		.success(function(docs) {
 			$scope.classes = docs;
+
+			$timeout(function() {
+				$scope.displayPage = true;
+				//console.log(angular.element('#app'));
+				angular.element('#app').attr('style', 'display: block;');
+			}, 2000);
+
 		})
-		.error(function(err){
+		.error(function(err) {
 			console.log(err);
 		});
 
-		$scope.showDetails = function(_class){
-			$scope.class = _class;
-			$scope.showClass = true;
+	$scope.showDetails = function(_class) {
+		$scope.class = _class;
+		$scope.showClass = true;
 
-			$http.get('/getclassRegbyClassid/' + _class._id)
+		$http.get('/getclassRegbyClassid/' + _class._id)
 			.success(function(docs) {
 				$scope.classReg = docs;
 			})
-			.error(function(err){
+			.error(function(err) {
 				console.log(err);
 			});
-		}
+	}
 
 });

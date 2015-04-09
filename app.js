cce05558f2ee425aa3a9281e6d01dcbc2de71d17
Umar bkh <umar482@gmail.com>
2015-04-09 17:45:48 +0500

    i18n switch added

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var mongoose = require('mongoose');
-var routes = require('./routes/index');
+var i18n = require('i18n-2')
+// var routes = require('./routes/index');
+
+// i18n.configure({
+//   // setup some locales - other locales default to en silently
+//   locales: ['en', 'iw'],
+//   defaultLocale: 'iw',
+//   // sets a custom cookie name to parse locale settings from
+//   cookie: 'lang',
+ 
+//   // where to store json files - defaults to './locales'
+//   directory: __dirname + '/locales'
+// });
+
 
 //connect to db
-var db = mongoose.connect('mongodb://localhost:27017/StudentTeacherDB');
-// var db = mongoose.connect('mongodb://nodejitsu:827578fcbfc33e31bb2ef44d1342e88c@troup.mongohq.com:10066/nodejitsudb3278735468');
+// var db = mongoose.connect('mongodb://localhost:27017/StudentTeacherDB');
+var db = mongoose.connect('mongodb://nodejitsu:827578fcbfc33e31bb2ef44d1342e88c@troup.mongohq.com:10066/nodejitsudb3278735468');
 
 var app = express();
 
@@ -33,11 +46,24 @@ app.use(flash());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('secret'));
+// app.use(express.cookieParser())
+//app.use(i18n.init);
+// i18n.setLocale('iw');
+
+i18n.expressBind(app, {
+  // setup some locales - other locales default to vi silently
+  locales: ['en', 'ch', 'ur', 'iw'],
+  // set the default locale
+  defaultLocale: 'ch',
+  // set the cookie name
+  cookieName: 'locale'
+});
 
 app.use(function(req, res, next) {
 
@@ -58,12 +84,30 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(function(req, res, next) {
+  req.i18n.setLocaleFromQuery();
+  req.i18n.setLocaleFromCookie();
+  next();
+});
+
+// language helper
+app.use(function(req, res, next) {
+    // express helper for natively supported engines
+    // res.locals.__ = res.__ = function() {
+    //     return i18n.__.apply(req, arguments);
+    // };
+    //res.locals.__ = req.i18n.__;
+    // i18n.setLocale(req, 'iw');
+    // console.log('hit');
+    next();
+});
+
 //routes
-app.use('/', routes);
+// app.use('/', routes);
+require('./routes/index')(app);
 require('./routes/users')(app);
 require('./routes/course')(app);
 require('./routes/classReg')(app);
-require('./routes/addeditclass')(app);
 require('./routes/mainPage')(app);
 require('./routes/wav')(app);
 
@@ -88,8 +132,6 @@ app.use(function(req, res, next) {
     req.db = db;
     next();
 });
-
-
 
 /// error handlers
 
--- routes/index.js
+++ routes/index.js
@@ -1,367 +1,383 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
+module.exports = function(app) {
 
-	if (req.param('msg')) {
-		res.locals.serverMsg = 'msg here';
-	} else {
-		res.locals.serverMsg = null;
-	}
+    /* GET home page. */
+    app.get('/', function(req, res) {
 
-
-	res.locals.logged = 0;
-	res.locals.isAdmin = req.session.isAdmin;
-
-	if (req.isAuthenticated()) {
-
-		res.locals.logged = 1;
-
-		//getClassRegs(req.user._id, function(docs){
-			//console.log(docs);
-		//});
-		
-
-		res.render('index', {
-			title: 'Home',
-			heading: 'Main page',
-			user: req.user,
-			// docs: docs
-		});
-
-	} else {
-		res.render('index', {
-			title: 'Home',
-			heading: 'Main page'
-		});
-	}
-
-});
+        if (req.param('msg')) {
+            res.locals.serverMsg = 'msg here';
+        } else {
+            res.locals.serverMsg = null;
+        }
 
 
-/* get Class reg info for main page */
-function getClassRegs(userID, cb){
+        res.locals.logged = 0;
+        res.locals.isAdmin = req.session.isAdmin;
 
-	var _Class = require('../models/class');
-	var User = require('../models/user');
-	var ClassReg = require('../models/classRegistration');
+        if (req.isAuthenticated()) {
 
-	User.findOne({
-			'_id': userID
-		}, function(err, user) {
-			if (err) {
-				res.send('User not Found');
-			} else {
-				if (user.local.userType == 'teacher') {
-					ClassReg.find({
-						'teachers': {
-							$in: [
-								userID
-							]
-						}
-					}, function(err, docs) {
-						if (err) {
-							// res.send(err);
-							// return err;
-							cb(err);
-						} else if (docs) {
+            res.locals.logged = 1;
 
-							// //make array of ids
-							var class_ids = [];
-							docs.forEach(function(item){
-								class_ids.push(item.class_id);
-							});
+            res.render('index', {
+                title: 'Home',
+                heading: 'Main page',
+                user: req.user,
+                // docs: docs
+            });
 
-							// //find all the class details
-							_Class.find({
-								'_id': {
-									$in: class_ids
-								}
-							}, function(err, docs) {
-								if (err) {
-									// res.send(err);
-									// return err;
-									cb(err);
-								} else {
-									// res.send(docs);
-									// return docs;									
-									cb(docs);
-								}
-							});
-						} else {
-							// res.send('[]');
-							cb('[]');
-						}
-					});
-				} else if (user.local.userType == 'student') {
+        } else {
 
-					console.log(userID);
-					//get student classes regs
-					ClassReg.find({
-						'students': {
-							$in: [
-								userID
-							]
-						}
-					}, function(err, docs) {
-						console.log(err);
-						if (err) {
-							// res.send(err);
-							// return err;
-							cb(err);
-						} else if (docs) {
-								// console.log('st');
-								console.log(docs);
-							// //make array of ids
-							var class_ids = [];
-							docs.forEach(function(item){
-								class_ids.push(item.class_id);
-							});
+            // req.i18n.setLocale('ur');
 
-							// //find all the class details
-							_Class.find({
-								'_id': {
-									$in: class_ids
-								}
-							}, function(err, docs) {
-								if (err) {
-									// res.send(err);
-									// return err;
-									cb(err);
-								} else {
-									//res.send(docs);
-									// return docs;
-									cb(docs);
-								}
-							});
-						} else {
-							// res.send('[]');
-							// return '[]';
-							cb('[]');
-						}
-					});
-				}
-			}
-		});
+            // or set it via the cookie
+  			res.cookie('locale', 'ch');
+  			req.i18n.setLocaleFromCookie();
+
+            // console.log(req.i18n.__('Hello i18n'));
+            res.render('index', {
+                title: 'Home',
+                heading: 'Main page'
+            });
+        }
+
+    });
+
+    // change language
+    app.get('/changeLang/:lang', function(req, res){
+    	var lang =  req.param('lang');
+
+    	res.cookie('locale', lang);
+		req.i18n.setLocaleFromCookie();
+
+		res.redirect('back');
+    });
+
+    /* get Class reg info for main page */
+    function getClassRegs(userID, cb) {
+
+        var _Class = require('../models/class');
+        var User = require('../models/user');
+        var ClassReg = require('../models/classRegistration');
+
+        User.findOne({
+            '_id': userID
+        }, function(err, user) {
+            if (err) {
+                res.send('User not Found');
+            } else {
+                if (user.local.userType == 'teacher') {
+                    ClassReg.find({
+                        'teachers': {
+                            $in: [
+                                userID
+                            ]
+                        }
+                    }, function(err, docs) {
+                        if (err) {
+                            // res.send(err);
+                            // return err;
+                            cb(err);
+                        } else if (docs) {
+
+                            // //make array of ids
+                            var class_ids = [];
+                            docs.forEach(function(item) {
+                                class_ids.push(item.class_id);
+                            });
+
+                            // //find all the class details
+                            _Class.find({
+                                '_id': {
+                                    $in: class_ids
+                                }
+                            }, function(err, docs) {
+                                if (err) {
+                                    // res.send(err);
+                                    // return err;
+                                    cb(err);
+                                } else {
+                                    // res.send(docs);
+                                    // return docs;									
+                                    cb(docs);
+                                }
+                            });
+                        } else {
+                            // res.send('[]');
+                            cb('[]');
+                        }
+                    });
+                } else if (user.local.userType == 'student') {
+
+                    console.log(userID);
+                    //get student classes regs
+                    ClassReg.find({
+                        'students': {
+                            $in: [
+                                userID
+                            ]
+                        }
+                    }, function(err, docs) {
+                        console.log(err);
+                        if (err) {
+                            // res.send(err);
+                            // return err;
+                            cb(err);
+                        } else if (docs) {
+                            // console.log('st');
+                            console.log(docs);
+                            // //make array of ids
+                            var class_ids = [];
+                            docs.forEach(function(item) {
+                                class_ids.push(item.class_id);
+                            });
+
+                            // //find all the class details
+                            _Class.find({
+                                '_id': {
+                                    $in: class_ids
+                                }
+                            }, function(err, docs) {
+                                if (err) {
+                                    // res.send(err);
+                                    // return err;
+                                    cb(err);
+                                } else {
+                                    //res.send(docs);
+                                    // return docs;
+                                    cb(docs);
+                                }
+                            });
+                        } else {
+                            // res.send('[]');
+                            // return '[]';
+                            cb('[]');
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    /* GET Admin page */
+    app.get('/admin', function(req, res) {
+        if (req.session.isAdmin && req.isAuthenticated()) {
+            res.locals.logged = 1;
+            res.locals.isAdmin = req.session.isAdmin;
+            res.render('adminpanel', {
+                title: 'Admin',
+                heading: 'Admin actions',
+                user: req.user
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+
+    // Admin Evaluators Page
+    app.get('/manageEvaluators', function(req, res) {
+        if (req.session.isAdmin && req.isAuthenticated()) {
+            res.locals.logged = 1;
+            res.locals.isAdmin = req.session.isAdmin;
+            res.render('manageevaluators', {
+                title: 'Manage Evaluators',
+                heading: '',
+                user: req.user
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+
+    /* GET Admin create class */
+    app.get('/createcourse', function(req, res) {
+        if (req.session.isAdmin && req.isAuthenticated()) {
+            res.locals.logged = 1;
+            res.locals.isAdmin = req.session.isAdmin;
+
+            var common = require("../config/common");
+
+            res.render('createcourse', {
+                title: 'Admin Create Course',
+                heading: '',
+                user: req.user,
+                common: common
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+
+    /* GET Admin edit class */
+    app.get('/editcourse', function(req, res) {
+        if (req.session.isAdmin && req.isAuthenticated()) {
+
+            var common = require("../config/common");
+
+            res.locals.logged = 1;
+            res.locals.isAdmin = req.session.isAdmin;
+            res.render('editcourses', {
+                title: 'Admin Edit Course',
+                heading: '',
+                user: req.user,
+                common: common
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+
+    /* GET Admin add edit class */
+    app.get('/addeditclass', function(req, res) {
+        if (req.session.isAdmin && req.isAuthenticated()) {
+
+            var common = require("../config/common");
+
+            res.locals.logged = 1;
+            res.locals.isAdmin = req.session.isAdmin;
+            res.render('addeditclass', {
+                title: 'Admin Add/Edit Class',
+                heading: '',
+                user: req.user,
+                common: common
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+
+    /* GET Admin create class */
+    app.get('/classreg', function(req, res) {
+        if (req.session.isAdmin && req.isAuthenticated()) {
+            res.locals.isAdmin = req.session.isAdmin;
+            res.locals.logged = 1;
+            res.render('classreg', {
+                title: 'Class Registration',
+                heading: '',
+                user: req.user
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+
+    // router.get('/editclass/:classid', function(req, res){
+    // 	if(req.session.isAdmin && req.isAuthenticated()){
+
+    // 		//get class 
+    // 		var class_id = req.param('classid');
+
+    // 		res.locals.logged = 1;
+    // 		res.render('editclass', {
+    // 			title: 'Admin Edit Class',
+    // 			heading: '',
+    // 			user: req.user,
+    // 			class_id: class_id
+    // 		});
+    // 	} else {
+    // 		res.redirect('/');
+    // 	}
+    // });
+
+    app.get('/classroom/:id', function(req, res) {
+
+        res.locals.class_id = req.param('id');
+        res.locals.logged = 0;
+        res.locals.isAdmin = req.session.isAdmin;
+
+        if (req.isAuthenticated()) {
+            res.locals.logged = 1;
+            res.render('classroom', {
+                title: 'Class Room',
+                heading: 'Class Room page',
+                user: req.user
+            });
+        } else {
+            res.redirect('/');
+        }
+
+    });
+
+    /* GET home page. */
+    app.get('/register', function(req, res) {
+
+        res.locals.logged = 0;
+        res.locals.isAdmin = req.session.isAdmin;
+
+        if (req.isAuthenticated()) {
+            res.locals.logged = 1;
+            res.redirect('/');
+        } else {
+            res.render('signup', {
+                title: 'Register',
+                heading: 'Registration page'
+            });
+        }
+
+    });
+
+    /* GET home page. */
+    app.get('/auth', function(req, res) {
+
+        res.locals.logged = 0;
+        res.locals.isAdmin = req.session.isAdmin;
+
+        if (req.isAuthenticated()) {
+            res.locals.logged = 1;
+            res.redirect('/');
+        } else {
+            res.render('auth', {
+                title: 'Auth',
+                heading: 'Register and Login'
+            });
+        }
+
+    });
+
+    app.get('/editprofile', function(req, res) {
+
+        var User = require('../models/user');
+
+        res.locals.logged = 0;
+        res.locals.isAdmin = req.session.isAdmin;
+
+        if (req.isAuthenticated()) {
+            res.locals.logged = 1;
+
+            //get logged in user
+            User.findOne({
+                _id: req.user._id
+            }, function(err, user) {
+                if (err) {
+                    res.send(err);
+                } else if (user) {
+
+                    var filesPath = './public/uploads/' + req.user._id + '/files/';
+
+                    //get user files
+                    var fs = require('fs');
+                    fs.readdir(filesPath, function(err, data) {
+                        res.render('editprofile', {
+                            title: 'Edit Profile',
+                            heading: 'Change Information',
+                            user: req.user,
+                            files: data
+                        });
+                    });
+
+
+                }
+            });
+
+
+
+        } else {
+            res.redirect('/');
+        }
+    });
+
 }
 
-/* GET Admin page */
-router.get('/admin', function(req, res) {
-	if (req.session.isAdmin && req.isAuthenticated()) {
-		res.locals.logged = 1;
-		res.locals.isAdmin = req.session.isAdmin;
-		res.render('adminpanel', {
-			title: 'Admin',
-			heading: 'Admin actions',
-			user: req.user
-		});
-	} else {
-		res.redirect('/');
-	}
-});
 
-// Admin Evaluators Page
-router.get('/manageEvaluators', function(req, res){
-	if (req.session.isAdmin && req.isAuthenticated()) {
-		res.locals.logged = 1;
-		res.locals.isAdmin = req.session.isAdmin;
-		res.render('manageevaluators', {
-			title: 'Manage Evaluators',
-			heading: '',
-			user: req.user
-		});
-	} else {
-		res.redirect('/');
-	}
-});
-
-/* GET Admin create class */
-router.get('/createcourse', function(req, res) {
-	if (req.session.isAdmin && req.isAuthenticated()) {
-		res.locals.logged = 1;
-		res.locals.isAdmin = req.session.isAdmin;
-
-		var common = require("../config/common");
-
-		res.render('createcourse', {
-			title: 'Admin Create Course',
-			heading: '',
-			user: req.user,
-			common: common
-		});
-	} else {
-		res.redirect('/');
-	}
-});
-
-/* GET Admin edit class */
-router.get('/editcourse', function(req, res) {
-	if (req.session.isAdmin && req.isAuthenticated()) {
-
-		var common = require("../config/common");
-
-		res.locals.logged = 1;
-		res.locals.isAdmin = req.session.isAdmin;
-		res.render('editcourses', {
-			title: 'Admin Edit Course',
-			heading: '',
-			user: req.user,
-			common: common
-		});
-	} else {
-		res.redirect('/');
-	}
-});
-
-/* GET Admin add edit class */
-router.get('/addeditclass', function(req, res) {
-	if (req.session.isAdmin && req.isAuthenticated()) {
-
-		var common = require("../config/common");
-
-		res.locals.logged = 1;
-		res.locals.isAdmin = req.session.isAdmin;
-		res.render('addeditclass', {
-			title: 'Admin Add/Edit Class',
-			heading: '',
-			user: req.user,
-			common: common
-		});
-	} else {
-		res.redirect('/');
-	}
-});
-
-/* GET Admin create class */
-router.get('/classreg', function(req, res) {
-	if (req.session.isAdmin && req.isAuthenticated()) {
-		res.locals.isAdmin = req.session.isAdmin;
-		res.locals.logged = 1;
-		res.render('classreg', {
-			title: 'Class Registration',
-			heading: '',
-			user: req.user
-		});
-	} else {
-		res.redirect('/');
-	}
-});
-
-// router.get('/editclass/:classid', function(req, res){
-// 	if(req.session.isAdmin && req.isAuthenticated()){
-
-// 		//get class 
-// 		var class_id = req.param('classid');
-
-// 		res.locals.logged = 1;
-// 		res.render('editclass', {
-// 			title: 'Admin Edit Class',
-// 			heading: '',
-// 			user: req.user,
-// 			class_id: class_id
-// 		});
-// 	} else {
-// 		res.redirect('/');
-// 	}
-// });
-
-router.get('/classroom/:id', function(req, res) {
-
-	res.locals.class_id = req.param('id');
-	res.locals.logged = 0;
-	res.locals.isAdmin = req.session.isAdmin;
-
-	if (req.isAuthenticated()) {
-		res.locals.logged = 1;
-		res.render('classroom', {
-			title: 'Class Room',
-			heading: 'Class Room page',
-			user: req.user
-		});
-	} else {
-		res.redirect('/');
-	}
-
-});
-
-/* GET home page. */
-router.get('/register', function(req, res) {
-
-	res.locals.logged = 0;
-	res.locals.isAdmin = req.session.isAdmin;
-
-	if (req.isAuthenticated()) {
-		res.locals.logged = 1;
-		res.redirect('/');
-	} else {
-		res.render('signup', {
-			title: 'Register',
-			heading: 'Registration page'
-		});
-	}
-
-});
-
-/* GET home page. */
-router.get('/auth', function(req, res) {
-
-	res.locals.logged = 0;
-	res.locals.isAdmin = req.session.isAdmin;
-
-	if (req.isAuthenticated()) {
-		res.locals.logged = 1;
-		res.redirect('/');
-	} else {
-		res.render('auth', {
-			title: 'Auth',
-			heading: 'Register and Login'
-		});
-	}
-
-});
-
-router.get('/editprofile', function(req, res) {
-
-	var User = require('../models/user');
-
-	res.locals.logged = 0;
-	res.locals.isAdmin = req.session.isAdmin;
-
-	if (req.isAuthenticated()) {
-		res.locals.logged = 1;
-
-		//get logged in user
-		User.findOne({
-			_id: req.user._id
-		}, function(err, user) {
-			if (err) {
-				res.send(err);
-			} else if (user) {
-
-				var filesPath = './public/uploads/'+req.user._id+'/files/';
-
-				//get user files
-				var fs = require('fs');
-				fs.readdir(filesPath, function(err, data){
-					res.render('editprofile', {
-						title: 'Edit Profile',
-						heading: 'Change Information',
-						user: req.user,
-						files: data
-					});	
-				});
-
-				
-			}
-		});
-
-
-
-	} else {
-		res.redirect('/');
-	}
-});
-
-
-module.exports = router;
+// module.exports = router;
